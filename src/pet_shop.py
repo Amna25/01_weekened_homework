249ab4320296848f1c5b11c5eb7bb33b5abeec91
Amna Bashir <amna.bashir2@gmail.com>
2021-11-21 14:04:17 +0000

    ok

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -2,5 +2,14 @@
 def get_pet_shop_name(name):
     return pet_shop[name]
 
+#no:2
+def get_total_cash(people):
+    get_total_cash= 0
+    for cash in people:
+        get_total_cash+= people["cash"]
+        if sum == total_cash:
+            print (sum)
+    
+
 
     
