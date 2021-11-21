dc9c00e11f047bd9a6cb61549ce14fdb56c296b2
Amna Bashir <amna.bashir2@gmail.com>
2021-11-21 20:04:44 +0000

    done

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,15 +1,48 @@
 # WRITE YOUR FUNCTIONS HERE
 def get_pet_shop_name(name):
-    return pet_shop[name]
+    return name["name"]
 
 #no:2
-def get_total_cash(people):
-    get_total_cash= 0
-    for cash in people:
-        get_total_cash+= people["cash"]
-        if sum == total_cash:
-            print (sum)
+def get_total_cash(sum):
+    return sum["admin"]["total_cash"]
+#no3
+def add_or_remove_cash(cash,add):
+     if add > 0:
+        x=cash["admin"]["total_cash"]+add
+        cash["admin"]["total_cash"]=x
+        return x
+     else :
+        x=cash["admin"]["total_cash"]+add
+        cash["admin"]["total_cash"]=x
+        return x
     
+#no4
+def get_pets_sold(sold):
+    return sold["admin"]["pets_sold"]
+#no5
+def increase_pets_sold(sold, value):
+    x=sold["admin"]["pets_sold"]+value
+    sold["admin"]["pets_sold"]=x
+#no6
+def get_stock_count(count):
+    x=len(count["pets"])
+    return x
+#no7
+def get_pets_by_breed(pet_shop,animal):
+    x = len(animal["pets"])
+    return x
+    
+
+
+
+
+
+        
+
+
+
+
+
 
 
     
